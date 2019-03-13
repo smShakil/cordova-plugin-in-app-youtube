5523dff1fe83ee0ec2a95d3398d172536dd36c37
S. M. Shakil <smshakil.kuet@gmail.com>
2019-03-13 13:26:54 +0600

    Exit from app on finish video

--- src/android/com/davidefavia/InAppYouTube.java
+++ src/android/com/davidefavia/InAppYouTube.java
@@ -51,7 +51,7 @@ public class InAppYouTube extends CordovaPlugin {
     String type;
     try {
       appIntent.putExtra("force_fullscreen", isFullScreen);
-      appIntent.putExtra("rel", 0);
+      appIntent.putExtra("finish_on_ended", true);
       this.cordova.getActivity().startActivity(appIntent);
       type = "application";
     } catch (ActivityNotFoundException ex) {
